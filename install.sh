0ef81d0921fb9af08d45d49429ab401ede58e841
casjay <casjay@yahoo.com>
2020-09-09 15:49:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-mkdir -p /srv/docker/registry && chmod -Rf 777 /srv/docker/registry
+APPNAME="registry"
+DATADIR="/srv/docker/$APPNAME"
 
+mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
+
+if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
+docker pull registry:v2 && docker restart $APPNAME
+else
 docker run -d \
 -p 5000:5000 \
 --restart=always \
 --name registry \
--v /srv/docker/registry:/var/lib/registry \
--e SEARCH_BACKEND=sqlalchemy \
+-v $DATADIR:/var/lib/registry \
 -v /etc/ssl/CA:/etc/ssl/CA \
 -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/ssl/CA/CasjaysDev/certs/localhost.crt \
 -e REGISTRY_HTTP_TLS_KEY=/etc/ssl/CA/CasjaysDev/private/localhost.key \
+-e SEARCH_BACKEND=sqlalchemy \
 registry
+fi
