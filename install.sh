df14bdadc00fc6065cd22c4a83ccf8dd57190b3d
Jason <jason@casjay.net>
2021-01-29 23:28:03 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 APPNAME="registry"
-DATADIR="/srv/docker/$APPNAME"
 
 mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker stop $APPNAME
-docker rm -f $APPNAME
-docker pull registry
+  docker stop "$APPNAME"
+  docker rm -f "$APPNAME"
+  docker pull registry
+else
+  docker run -d \
+    -p 5000:5000 \
+    --restart=always \
+    --name "$APPNAME" \
+    -v "$DATADIR":/var/lib/registry \
+    -v /etc/ssl/CA:/etc/ssl/CA \
+    -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/ssl/CA/CasjaysDev/certs/localhost.crt \
+    -e REGISTRY_HTTP_TLS_KEY=/etc/ssl/CA/CasjaysDev/private/localhost.key \
+    -e SEARCH_BACKEND=sqlalchemy \
+    registry
 fi
-docker run -d \
--p 5000:5000 \
---restart=always \
---name "$APPNAME" \
--v $DATADIR:/var/lib/registry \
--v /etc/ssl/CA:/etc/ssl/CA \
--e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/ssl/CA/CasjaysDev/certs/localhost.crt \
--e REGISTRY_HTTP_TLS_KEY=/etc/ssl/CA/CasjaysDev/private/localhost.key \
--e SEARCH_BACKEND=sqlalchemy \
-registry
