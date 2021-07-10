eaa3fedbcb822502c938373de280215786abece5
Jason <jason@casjay.net>
2021-07-10 15:55:01 -0400

    🐛 FIXED: install.sh 🐛

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 APPNAME="registry"
+DOCKER_HUB_URL="registry"
 
-mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
+sudo mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-  docker stop "$APPNAME"
-  docker rm -f "$APPNAME"
-  docker pull registry
+  sudo docker stop "$APPNAME"
+  sudo docker rm -f "$APPNAME"
+  sudo docker pull "$DOCKER_HUB_URL"
+  sudo docker restart "$APPNAME"
 else
-  docker run -d \
+  sudo docker run -d \
     -p 5000:5000 \
     --restart=always \
     --name "$APPNAME" \
@@ -18,5 +20,5 @@ else
     -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/ssl/CA/CasjaysDev/certs/localhost.crt \
     -e REGISTRY_HTTP_TLS_KEY=/etc/ssl/CA/CasjaysDev/private/localhost.key \
     -e SEARCH_BACKEND=sqlalchemy \
-    registry
+    "$DOCKER_HUB_URL"
 fi
