bd32eead3bfa70e010a02023d63a1358794624df
casjay <casjay@yahoo.com>
2021-08-26 10:52:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ HOME="${USER_HOME:-${HOME}}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #set opts
+exit 1
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version       : 202107311147-git
@@ -40,51 +41,75 @@ fi
 # user system devenv dfmgr dockermgr fontmgr iconmgr pkmgr systemmgr thememgr wallpapermgr
 dockermgr_install
 __options "$@"
+__sudo() { if sudo -n true; then eval sudo "$*"; else eval "$*"; fi; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Begin installer
 APPNAME="registry"
-DOCKER_HUB_URL="registry"
+DOCKER_HUB_URL="registry/registry"
+REGISTRY_SERVER_PORT="${REGISTRY_SERVER_PORT:-5000}"
+REGISTRY_SERVER_HOST="${REGISTRY_SERVER_HOST:-$(hostname -f 2>/dev/null)}"
+REPO="${DOCKERMGRREPO:-https://github.com/dockermgr}/$APPNAME"
+REPO_BRANCH="${GIT_REPO_BRANCH:-main}"
+REGISTRY_SERVER_TIMEZONE="${TZ:-${TIMEZONE:-America/New_York}}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPDIR="/usr/local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
-INSTDIR="/usr/local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
-DATADIR="/srv/docker/$APPNAME"
-REPORAW="$REPO/raw/$GIT_DEFAULT_BRANCH"
+if user_is_root; then
+  APPDIR="$CASJAYSDEVDIR/$SCRIPTS_PREFIX/$APPNAME"
+  INSTDIR="$CASJAYSDEVDIR/$SCRIPTS_PREFIX/$APPNAME"
+  DATADIR="/srv/docker/$APPNAME"
+else
+  APPDIR="$HOME/.local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
+  INSTDIR="$HOME/.local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
+  DATADIR="$HOME/.local/share/srv/docker/$APPNAME"
+fi
+REPORAW="$REPO/raw/$REPO_BRANCH"
 APPVERSION="$(__appversion "$REPORAW/version.txt")"
-TIMEZONE="${TZ:-$TIMEZONE}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-sudo mkdir -p "$DATADIR"/{data}
-sudo chmod -Rf 777 "$DATADIR"
+__sudo mkdir -p "$DATADIR/data"
+__sudo mkdir -p "$DATADIR/config"
+__sudo chmod -Rf 777 "$DATADIR"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-if [ -f "$INSTDIR/docker-compose.yml" ]; then
+if [ -f "$INSTDIR/docker-compose.yml" ] && cmd_exists docker-compose; then
   printf_blue "Installing containers using docker compose"
   sed -i "s|REPLACE_DATADIR|$DATADIR" "$INSTDIR/docker-compose.yml"
   if cd "$INSTDIR"; then
-    sudo docker-compose pull &>/dev/null
-    sudo docker-compose up -d &>/dev/null
+    __sudo docker-compose pull &>/dev/null
+    __sudo docker-compose up -d &>/dev/null
   fi
 else
-  if docker ps -a | grep -qs "$APPNAME"; then
-    sudo docker pull "$DOCKER_HUB_URL" &>/dev/null
-    sudo docker restart "$APPNAME" &>/dev/null
+  if docker ps -a | grep -qsw "$APPNAME"; then
+    __sudo docker pull "$DOCKER_HUB_URL" &>/dev/null
+    __sudo docker restart "$APPNAME" &>/dev/null
   else
-    sudo docker run -d \
+    __sudo docker run -d \
       --name="$APPNAME" \
       --hostname "$APPNAME" \
       --restart=unless-stopped \
       --privileged \
-      -e TZ=${TIMEZONE:-America/New_York} \
-      -v "$DATADIR/data":/var/lib/registry:z \
-      #-v /etc/ssl/CA:/etc/ssl/CA \
-      #-e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/ssl/CA/CasjaysDev/certs/localhost.crt \
-      #-e REGISTRY_HTTP_TLS_KEY=/etc/ssl/CA/CasjaysDev/private/localhost.key \
       -e SEARCH_BACKEND=sqlalchemy \
-      -p 5000:5000 \
+      -e TZ="$REGISTRY_SERVER_TIMEZONE" \
+      -v "$DATADIR/data":/var/lib/registry:z \
+      -p "$REGISTRY_SERVER_PORT":5000 \
       "$DOCKER_HUB_URL" &>/dev/null
+    ## SSl Config
+    # __sudo docker run -d \
+    #   --name="$APPNAME" \
+    #   --hostname "$APPNAME" \
+    #   --restart=unless-stopped \
+    #   --privileged \
+    #   -e SEARCH_BACKEND=sqlalchemy \
+    #   -v /etc/ssl/CA:/etc/ssl/CA \
+    #   -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/ssl/CA/CasjaysDev/certs/localhost.crt \
+    #   -e REGISTRY_HTTP_TLS_KEY=/etc/ssl/CA/CasjaysDev/private/localhost.key \
+    #   -e TZ="$REGISTRY_SERVER_TIMEZONE" \
+    #   -v "$DATADIR/data":/data:z \
+    #   -v "$DATADIR/config":/config:z \
+    #   -p "$REGISTRY_SERVER_PORT":5000 \
+    #   "$DOCKER_HUB_URL" &>/dev/null
   fi
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if docker ps -a | grep -qs "$APPNAME"; then
-  printf_blue "Service is available at: http://$HOSTNAME:5000"
+  printf_blue "Service is available at: http://$REGISTRY_SERVER_HOST:$REGISTRY_SERVER_PORT"
 else
   false
 fi
