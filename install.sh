883dccffdb1c69925d570ed1ec7078c48081c63d
casjay <casjay@yahoo.com>
2021-08-28 17:52:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME="registry"
+APPNAME="registry-web"
 VERSION="202107311147-git"
 USER="${SUDO_USER:-${USER}}"
 HOME="${USER_HOME:-${HOME}}"
@@ -16,8 +16,8 @@ SRC_DIR="${BASH_SOURCE%/*}"
 # @ReadME        : dockermgr --help
 # @Copyright     : Copyright: (c) 2021 casjay, casjay
 # @Created       : Saturday, Jul 31, 2021 11:47 EDT
-# @File          : registry
-# @Description   : registry docker container installer
+# @File          : registry-web
+# @Description   : registry-web docker container installer
 # @TODO          :
 # @Other         :
 # @Resource      :
@@ -43,13 +43,13 @@ __options "$@"
 __sudo() { if sudo -n true; then eval sudo "$*"; else eval "$*"; fi; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Begin installer
-APPNAME="registry"
-DOCKER_HUB_URL="registry/registry"
-REGISTRY_SERVER_PORT="${REGISTRY_SERVER_PORT:-5000}"
-REGISTRY_SERVER_HOST="${REGISTRY_SERVER_HOST:-$(hostname -f 2>/dev/null)}"
+APPNAME="registry-web"
+DOCKER_HUB_URL="konradkleine/docker-registry-frontend:v2"
+REGISTRY_WEB_SERVER_PORT="${REGISTRY_WEB_SERVER_PORT:-7080}"
+REGISTRY_WEB_SERVER_HOST="${REGISTRY_WEB_SERVER_HOST:-$(hostname -f 2>/dev/null)}"
 REPO="${DOCKERMGRREPO:-https://github.com/dockermgr}/$APPNAME"
 REPO_BRANCH="${GIT_REPO_BRANCH:-main}"
-REGISTRY_SERVER_TIMEZONE="${TZ:-${TIMEZONE:-America/New_York}}"
+REGISTRY_WEB_SERVER_TIMEZONE="${TZ:-${TIMEZONE:-America/New_York}}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if user_is_root; then
   APPDIR="$CASJAYSDEVDIR/$SCRIPTS_PREFIX/$APPNAME"
@@ -64,7 +64,6 @@ REPORAW="$REPO/raw/$REPO_BRANCH"
 APPVERSION="$(__appversion "$REPORAW/version.txt")"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 __sudo mkdir -p "$DATADIR/data"
-__sudo mkdir -p "$DATADIR/config"
 __sudo chmod -Rf 777 "$DATADIR"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if [ -f "$INSTDIR/docker-compose.yml" ] && cmd_exists docker-compose; then
@@ -76,8 +75,25 @@ if [ -f "$INSTDIR/docker-compose.yml" ] && cmd_exists docker-compose; then
   fi
 else
   if docker ps -a | grep -qsw "$APPNAME"; then
-    __sudo docker pull "$DOCKER_HUB_URL" &>/dev/null
-    __sudo docker restart "$APPNAME" &>/dev/null
+    __sudo docker stop "$APPNAME" &>/dev/null
+    __sudo docker rm -f "$APPNAME" &>/dev/null
+  fi
+  if [ -f "/etc/ssl/CA/CasjaysDev/certs/localhost.crt" ] && [ -f "/etc/ssl/CA/CasjaysDev/private/localhost.key" ]; then
+    ## SSL
+    __sudo docker run -d \
+      --name="$APPNAME" \
+      --hostname "$APPNAME" \
+      --restart=unless-stopped \
+      --privileged \
+      -e SEARCH_BACKEND=sqlalchemy \
+      -v /etc/ssl/CA:/etc/ssl/CA \
+      -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/ssl/CA/CasjaysDev/certs/localhost.crt \
+      -e REGISTRY_HTTP_TLS_KEY=/etc/ssl/CA/CasjaysDev/private/localhost.key \
+      -e TZ="$REGISTRY_SERVER_TIMEZONE" \
+      -v "$DATADIR/data":/data:z \
+      -v "$DATADIR/config":/config:z \
+      -p "$REGISTRY_SERVER_PORT":5000 \
+      "$DOCKER_HUB_URL" &>/dev/null
   else
     __sudo docker run -d \
       --name="$APPNAME" \
@@ -89,26 +105,11 @@ else
       -v "$DATADIR/data":/var/lib/registry:z \
       -p "$REGISTRY_SERVER_PORT":5000 \
       "$DOCKER_HUB_URL" &>/dev/null
-    ## SSl Config
-    # __sudo docker run -d \
-    #   --name="$APPNAME" \
-    #   --hostname "$APPNAME" \
-    #   --restart=unless-stopped \
-    #   --privileged \
-    #   -e SEARCH_BACKEND=sqlalchemy \
-    #   -v /etc/ssl/CA:/etc/ssl/CA \
-    #   -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/ssl/CA/CasjaysDev/certs/localhost.crt \
-    #   -e REGISTRY_HTTP_TLS_KEY=/etc/ssl/CA/CasjaysDev/private/localhost.key \
-    #   -e TZ="$REGISTRY_SERVER_TIMEZONE" \
-    #   -v "$DATADIR/data":/data:z \
-    #   -v "$DATADIR/config":/config:z \
-    #   -p "$REGISTRY_SERVER_PORT":5000 \
-    #   "$DOCKER_HUB_URL" &>/dev/null
   fi
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if docker ps -a | grep -qs "$APPNAME"; then
-  printf_blue "Service is available at: http://$REGISTRY_SERVER_HOST:$REGISTRY_SERVER_PORT"
+  printf_blue "Service is available at: http://$REGISTRY_WEB_SERVER_HOST:$REGISTRY_WEB_SERVER_PORT"
 else
   false
 fi
